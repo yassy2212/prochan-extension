3d193d4474a7539a1356225e2df5f2369a7b167a
yassy2212 <yassirbou30@gmail.com>
2025-12-31 02:14:59 +0100

    Update Prochan.kt

--- src/ar/prochan/Prochan.kt
+++ src/ar/prochan/Prochan.kt
@@ -5,3 +5,8 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 class Prochan : Madara("Prochan", "https://prochan.net", "ar") {
     override val mangaSubStringPath = "manga"
 }
+package eu.kanade.tachiyomi.extension.ar.prochan
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+class Prochan : Madara("Prochan", "https://prochan.net", "ar") {
+    override val mangaSubStringPath = "manga"
+}
